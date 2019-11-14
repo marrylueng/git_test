151d43cf3a7090b20bb553a750eb45fef3f7b6f6
marrylueng <marrylueng1212@163.com>
2019-11-14 17:36:53 +0800

    增加输出,push2

--- git_project_test/src/cn/wolfcode/Hello.java
+++ git_project_test/src/cn/wolfcode/Hello.java
@@ -13,9 +13,12 @@ public class Hello {
         System.out.println("hello 3");
         System.out.println("hello 3");
         System.out.println("function");
+
+        System.out.println("push2");
         put();
     }
     public static void put() {
         System.out.println("方法1");
+        System.out.println("push2");
     }
 }
